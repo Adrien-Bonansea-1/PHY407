342b80b6e8b0122081f6343d0feac17b4d275cf9
Adrien-Bonansea <adrien.bonansea@mail.utoronto.ca>
2025-09-08 17:11:54 -0400

    formatted

---
+++ Lab_1: Python_basics/Q1.py
@@ -0,0 +1,11 @@
+# Q1:
+# The equations requested in part A will be in the Questions_and_Explanations txt file
+# The Pseudo code for part B will be done in the Questions_and_Explanations txt file
+
+#imports
+import numpy as np
+import matplotlib as plt
+import scipy as sc
+
+#Part C
+
